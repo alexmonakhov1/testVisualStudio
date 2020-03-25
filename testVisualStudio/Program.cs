54da3f41a57c0cd27a2b239438e2dead98fd7795
Александр Монахов <alexmonakhov12@outlook.com>
2020-03-25 15:33:36 +0300

    first commit

---
+++ testVisualStudio/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+namespace testVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            Write(myClass.MainMethod());
+        }
+    }
+    class MyClass
+    {
+        public MyClass()
+        {
+
+        }
+        public MyClass(string myFavoriteLanguage)
+        {
+            language = myFavoriteLanguage;
+        }
+
+        string language = string.Empty;
+        public string MainMethod()
+        {
+            if (language != string.Empty)
+            {
+                return language + " is the best language";
+            }
+            return "The best language is C#";
+        }
+    }
+}
